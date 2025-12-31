acf277a53bbf3dc1f0312dbb4b407748d32bd42d
Andre Cristovam <andrecristovam@gmail.com>
2025-12-30 21:03:25 -0300

    chore: configuração inicial do projeto Spring Boot

---
+++ src/main/java/com/andrecristovam/desafiocoupon/DesafiocouponApplication.java
@@ -0,0 +1,13 @@
+package com.andrecristovam.desafiocoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiocouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiocouponApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrecristovam/desafiocoupon/DesafiocouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrecristovam.desafiocoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiocouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
